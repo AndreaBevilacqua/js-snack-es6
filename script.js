314ffda53bb6532f482b47c4f9c7c3fa46898865
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-01-16 15:46:31 +0100

    added first exercise milestones

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('js ok');
+
+// ^ ESERCIZIO 1
+
+// ^ MILESTONE - 1 Creo un array con tutti gli ospiti e un variabile tavolo vip
+// ^ MILESTONE - 2 Creo un array vuoto guestlist in cui inserisco i campi usando inoltre .map()
+// ^ MILESTONE - 3 Stampo in console i risultati
+
+const tableName = 'Tavolo Vip';
+
+const guests = ['Brad Pitt','Johnny Depp','Lady Gaga','Cristiano Ronaldo','Georgina Rodriguez','Chiara Ferragni','Fedez','George Clooney','Amal Clooney','Maneskin'];
