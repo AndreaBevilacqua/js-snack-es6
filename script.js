2e05006b2974841ae290baaa62e439508516c930
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-01-16 15:50:32 +0100

    added guestlist array

--- script.js
+++ script.js
@@ -6,6 +6,24 @@ console.log('js ok');
 // ^ MILESTONE - 2 Creo un array vuoto guestlist in cui inserisco i campi usando inoltre .map()
 // ^ MILESTONE - 3 Stampo in console i risultati
 
+// ~ ================
+// ~      ARRAY
+// ~ ================
+
 const tableName = 'Tavolo Vip';
 
 const guests = ['Brad Pitt','Johnny Depp','Lady Gaga','Cristiano Ronaldo','Georgina Rodriguez','Chiara Ferragni','Fedez','George Clooney','Amal Clooney','Maneskin'];
+
+// ? ===============
+// ?   SVOLGIMENTO
+// ? ===============
+
+const guestlist = guests.map((guest, i) => {
+    return {
+        tableName: tableName,
+        guestName: guest,
+        seatOccupied: i + 1
+    };
+});
+
+// Stampo in console
