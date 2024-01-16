7f9aaf2c1fe2404b2710107dbda8061e4c979890
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-01-16 16:00:43 +0100

    added second array

--- script.js
+++ script.js
@@ -36,3 +36,17 @@ console.log(guestlist);
 // ^ MILESTONE - 3 Stabilisco quali studenti hanno totalizzato un voto superiore a 70 e id superiore a 120
 // ^ MILESTONE - 4 Stampare in console infine un array con il nome di questi studenti tutto in maiuscolo
 
+// ~ ================
+// ~      ARRAY
+// ~ ================
+
+const students = [
+    { id: 213, name: 'Marco della Rovere', grade: 78 },
+    { id: 110, name: 'Paola Cortellessa', grade: 96 },
+    { id: 250, name: 'Andrea Mantegna', grade: 48 },
+    { id: 145, name: 'Gaia Borromini', grade: 74 },
+    { id: 196, name: 'Luigi Grimandello', grade: 68 },
+    { id: 102, name: 'Piero della Francesca', grade: 50 },
+    { id: 120, name: 'Francesca da Polenta', grade: 84 },
+]
+
